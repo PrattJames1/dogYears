6dcf4fb66fa7f9114a3cf0ecc46c24945ef1f16d
Professional <James@Jamess-MacBook-Pro.local>
2017-04-01 15:55:28 -0600

    Initial commit

---
+++ dogYears.js
@@ -0,0 +1,6 @@
+var myAge = prompt("What is your age? ");
+var firstYears = 2 * 10.5;
+var laterYears = (myAge - 2) * 4;
+var myAgeInDogYears = firstYears + laterYears;
+
+console.log("My age in dog years: " + myAgeInDogYears);
